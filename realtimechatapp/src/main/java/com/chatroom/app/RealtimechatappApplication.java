6418e5ac57a2b2c2d2966fc2edf36ddbe334216f
Sunny Kumar <83705127+sunnykumar2@users.noreply.github.com>
2022-01-19 18:06:55 +0530

    Add files via upload

---
+++ realtimechatapp/src/main/java/com/chatroom/app/RealtimechatappApplication.java
@@ -0,0 +1,13 @@
+package com.chatroom.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimechatappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimechatappApplication.class, args);
+	}
+
+}
---
+++ realtimechatapp/src/test/java/com/chatroom/app/RealtimechatappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatroom.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimechatappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
